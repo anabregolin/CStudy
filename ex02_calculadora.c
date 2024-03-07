d77f59c2abcbb4ed892734f0500ab9fd0e286af3
Ana Carolina <128608586+anabregolin@users.noreply.github.com>
2024-03-07 10:46:32 -0300

    Update ex02_calculadora.c

--- ex02_calculadora.c
+++ ex02_calculadora.c
@@ -1,19 +1,83 @@
 //aula 7/3/24
-/*calculadora de 4 opçoes
-soma sub multipl divisao
-
-3 valores
-
-int op
-float partela1
-float parcela 2
-
-opção inválida
-*/
 
 #include <stdio.h>
+#include <stdbool.h>
 #include <locale.h>
 
 void main(void){
-    printf("--- MENU --- \n (1) SOMA \n (2) SUBTRAÇÃO \n (3) MULTIPLICAÇÃO \n (4) DIVISÃO \n Insira a operação que deseja realizar: ");
+    int op;
+    float p1;
+    float p2;
+    bool i = true;
+    setlocale(LC_ALL, "Portuguese");
+    
+do{
+    printf("\n--- MENU --- \n (0) SOMA \n (1) SUBTRAÇÃO ");
+    printf("\n (2) MULTIPLICAÇÃO \n (3) DIVISÃO \n (4) SAIR \n Insira a operação que deseja realizar: ");
+    scanf("%d" , &op);
+
+    switch(op){
+        case 0:
+        printf("\n Informe a primeira parcela: ");
+        scanf("%f", &p1);
+
+        printf(" Informe a segunda parcela: ");
+        scanf("%f", &p2);
+
+        float soma = p1 + p2;
+        printf(" O valor %.2f é a soma total. \n", soma);
+        i = false;
+        break;
+        ///-----------
+
+        case 1:
+        printf("\n Informe a primeira parcela: ");
+        scanf("%f", &p1);
+
+        printf(" Informe a segunda parcela: ");
+        scanf("%f", &p2);
+
+        float sub = p1 - p2;
+        printf(" O valor %.2f é a subtração total. \n", sub);
+        i = false;
+        break;
+        ///-----------
+
+        case 2:
+        printf("\n Informe a primeira parcela: ");
+        scanf("%f", &p1);
+
+        printf(" Informe a segunda parcela: ");
+        scanf("%f", &p2);
+
+        float mult = p1 * p2;
+        printf(" O valor %.2f é a multiplicação total. \n", mult);
+        i = false;
+        break; 
+        ///-----------
+
+        case 3:
+        printf("\n Informe a primeira parcela: ");
+        scanf("%f", &p1);
+
+        printf(" Informe a segunda parcela: ");
+        scanf("%f", &p2);
+
+        float div = p1 / p2;
+        printf(" O valor %.2f é a divisão total. \n", div);
+        i = false;
+        break;
+        ///-----------
+
+        case 4:
+        printf("\n Saindo...\n");
+        i = false;
+        break;
+
+        default:
+        printf("\n ***Valor inválido, tente novamente.***\n");
+        break;
+
+    }
+}while(i);
 }
