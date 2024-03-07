ad88dc8804ca43693d90ca0811276e3254c7b8b3
Ana Carolina <128608586+anabregolin@users.noreply.github.com>
2024-03-07 10:11:22 -0300

    Create ex02_calculadora.c

---
+++ ex02_calculadora.c
@@ -0,0 +1,19 @@
+//aula 7/3/24
+/*calculadora de 4 opçoes
+soma sub multipl divisao
+
+3 valores
+
+int op
+float partela1
+float parcela 2
+
+opção inválida
+*/
+
+#include <stdio.h>
+#include <locale.h>
+
+void main(void){
+    printf("--- MENU --- \n (1) SOMA \n (2) SUBTRAÇÃO \n (3) MULTIPLICAÇÃO \n (4) DIVISÃO \n Insira a operação que deseja realizar: ");
+}
